f45c9ffaa76ad8afeed6eba3584afff89f3bc6f3
Carson Howard <cjhoward92@gmail.com>
2019-01-25 18:07:06 -0700

    Fix max subarry

--- maximum-subarray/main.c
+++ maximum-subarray/main.c
@@ -3,16 +3,16 @@
 #include <stddef.h>
 
 typedef struct {
-  size_t max_left;
-  size_t max_right;
-  size_t sum;
+  int max_left;
+  int max_right;
+  int sum;
 } subarray_t;
 
 subarray_t find_max_crossing(int arr[], int low, int mid, int high) {
   subarray_t sub = { 0, 0, 0 };
   
-  size_t left_sum = INT_MIN;
-  size_t sum = 0;
+  int left_sum = -100000;
+  int sum = 0;
 
   int i;
   for (i = mid; i >= low; i--) {
@@ -23,7 +23,7 @@ subarray_t find_max_crossing(int arr[], int low, int mid, int high) {
     }
   }
 
-  size_t right_sum = INT_MIN;
+  int right_sum = -100000;
   sum = 0;
 
   for (i = mid + 1; i <= high; i++) {
@@ -61,19 +61,19 @@ subarray_t find_max_subarray(int arr[], int low, int high) {
 int main() {
   int arr[10];
   arr[0] = -10;
-  arr[1] = 10;
-  arr[2] = 2;
+  arr[1] = -10;
+  arr[2] = -2;
   arr[3] = 0;
   arr[4] = 5;
-  arr[5] = 8;
+  arr[5] = 30;
   arr[6] = -9;
   arr[7] = 3;
-  arr[8] = 30;
+  arr[8] = -8;
   arr[9] = 1;
 
   subarray_t sub = find_max_subarray(arr, 0, 9);
 
-  printf("left: %ld; right: %ld; sum: %ld\n", sub.max_left, sub.max_right, sub.sum);
+  printf("left: %d; right: %d; sum: %d\n", sub.max_left, sub.max_right, sub.sum);
 
   return 0;
 }
